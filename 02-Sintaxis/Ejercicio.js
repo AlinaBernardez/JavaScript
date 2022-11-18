ba37ea5746dcefbcf6868cc8eb24ef39095c5d83
CorrePlatano13 <116650041+CorrePlatano13@users.noreply.github.com>
2022-11-18 13:35:33 +0100

    Add files via upload

---
+++ 02-Sintaxis/Ejercicio.js
@@ -0,0 +1,15 @@
+let nombre = "Alina Bernárdez";
+let edad = 33;
+let isDeveloper = true;
+const fechaNacimiento = new Date(1989, 00, 5);
+
+const libro = {
+    titulo: "Las hijas de la tierra",
+    autor: "Alaitz Leceaga",
+    anyo: 2019,
+    enlaceCompra: "https://www.amazon.es/Las-hijas-tierra-Grandes-novelas/dp/8466666338"
+}
+
+const ejercicio = [nombre, edad, isDeveloper, fechaNacimiento, libro];
+
+console.log(ejercicio);
