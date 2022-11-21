c86fb4c84af50196bca07d1bf81f434e548acf5c
CorrePlatano13 <116650041+CorrePlatano13@users.noreply.github.com>
2022-11-21 13:01:26 +0100

    Add files via upload

---
+++ ejercicioT3.js
@@ -0,0 +1,24 @@
+// Bucle for -- Factorial 10:
+
+const  number = 10;
+let fact = 1;
+
+
+for (let i = 1; i <= number; i++) {
+    fact = fact * i; 
+    console.log(fact)
+}
+
+// Bucle while:
+
+const number2 = 10;
+let fact2 = 1;
+let j = 1;
+
+while (j <= number) {
+    fact = fact * i;
+    j++;
+}
+
+// Bucle while + bifurcación if + break:
+
