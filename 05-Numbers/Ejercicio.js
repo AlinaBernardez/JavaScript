fdc8e79ae423923a1c9fa0de3797a523a1d4e09b
CorrePlatano13 <116650041+CorrePlatano13@users.noreply.github.com>
2022-11-24 14:12:11 +0100

    Add files via upload

---
+++ 05-Numbers/Ejercicio.js
@@ -0,0 +1,13 @@
+let altura = 173;
+let alturaMetro = 1.73;
+let peso = 63.5;
+
+let alturaRedond = alturaMetro.toPrecision(2);
+console.log(alturaRedond);
+
+let pesoRedond = (peso.toPrecision(2)) - 1;
+console.log(pesoRedond)
+
+let maxValor = (Number.MAX_VALUE + 1) 
+
+console.log(maxValor === Number.MAX_VALUE);
